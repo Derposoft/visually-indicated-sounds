8388c8036c8979506916be7f9f3b39c8ad4bb589
Viswanath Chadalapaka <viswanath.chadalapaka@gmail.com>
2023-10-02 21:33:49 -0700

    updated dataloader and video normalization

--- data/utils.py
+++ data/utils.py
@@ -12,8 +12,6 @@ import argparse
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import shutil
 
-root_url = "https://www.youtube.com/watch?v="
-
 
 def download_video(video_id: str, dirname: str):
     # URL of the YouTube video you want to download
@@ -24,55 +22,57 @@ def download_video(video_id: str, dirname: str):
     video_stream.download(output_path=dirname, filename=filename)
     return filename
 
+
 def download_data(train_dir: str, test_dir: str, num_train: int = 5, num_test: int = 2):
+    # Read video id data file
     max_videos = num_train + num_test
     video_id_file = os.path.join(os.path.dirname(__file__), "vig_dl.lst")
-
-    annotations_dict, classmap_dict = load_annotations_and_classmap()
-
+    annotations_dict, _ = load_annotations_and_classmap()
     with open(video_id_file) as fin:
         lines = fin.readlines()
-        for video_idx, line in tqdm(enumerate(lines[:max_videos])):
-            youtube_video_id, video_id = line.strip().split(",")
-            video_id = int(video_id)
 
-            download_dir = train_dir if video_idx < num_train else test_dir
+    for video_idx, line in tqdm(enumerate(lines[:max_videos])):
+        youtube_video_id, video_id = line.strip().split(",")
+        video_id = int(video_id)
+        download_dir = train_dir if video_idx < num_train else test_dir
 
-            try:
-                download_video_filename = download_video(youtube_video_id, download_dir)
-            except:
+        # Download video
+        try:
+            download_video_filename = download_video(youtube_video_id, download_dir)
+        except:
+            continue
+
+        # Clip video with ffmpeg if it is a snippet
+        download_video_saved_path = os.path.join(download_dir, download_video_filename)
+        clipped_video_saved_path = os.path.join(
+            download_dir, "clipped_" + download_video_filename
+        )
+        if video_id in annotations_dict:
+            annotation = annotations_dict[video_id]
+            start_label, end_label = "start_time", "end_time"
+            if start_label not in annotation or end_label not in annotation:
                 continue
+            start_time = int(annotation[start_label])
+            end_time = int(annotation[end_label])
+            ffmpeg_extract_subclip(
+                download_video_saved_path,
+                start_time,
+                end_time,
+                targetname=clipped_video_saved_path,
+            )
 
-            download_video_saved_path = os.path.join(download_dir, download_video_filename)
-            clipped_video_saved_path = os.path.join(download_dir, "clipped_" + download_video_filename)
-
-            print()
-
-            if video_id in annotations_dict:
-                desired_annotation = annotations_dict[video_id]
-
-                start_time = int(desired_annotation['start_time'])
-                end_time = int(desired_annotation['end_time'])
-
-                print("start_time:", start_time)
-                print("end_time:", end_time)
-
-                ffmpeg_extract_subclip(download_video_saved_path, start_time, end_time, targetname=clipped_video_saved_path)
-
-                os.remove(download_video_saved_path)
-                shutil.move(clipped_video_saved_path, download_video_saved_path)
+            # Replace original video with clipped video
+            os.remove(download_video_saved_path)
+            shutil.move(clipped_video_saved_path, download_video_saved_path)
 
 
-def download_data_if_not_downloaded(
-    data_dir=os.path.join(os.path.dirname(__file__), "./vig_train"),
-    n_train_videos=1000,
-    n_test_videos=200,
-):
-    data_files = [f for f in os.listdir(data_dir) if f.endswith(".mp4")]
+def download_data_if_not_downloaded(n_train_videos=1000, n_test_videos=200):
+    train_dir = os.path.join(os.path.dirname(__file__), "./vig_train")
+    test_dir = os.path.join(os.path.dirname(__file__), "./vig_test")
+    data_files = [f for f in os.listdir(train_dir) if f.endswith(".mp4")]
     if not data_files:
-        train_dir = os.path.join(os.path.dirname(__file__), "./vig_train")
-        test_dir = os.path.join(os.path.dirname(__file__), "./vig_test")
         download_data(train_dir, test_dir, n_train_videos, n_test_videos)
+
 
 @lru_cache(maxsize=None)
 def load_video_ids():
@@ -85,9 +85,9 @@ def load_video_ids():
     return map
 
 
-def video_id_to_dataset_id(video_id):
+def video_id_to_dataset_id(video_id: str) -> int:
     map = load_video_ids()
-    return map[video_id]
+    return int(map[video_id])
 
 
 def load_annotations_and_classmap():
@@ -104,48 +104,119 @@ def load_annotations_and_classmap():
     return annotations, class_map
 
 
+class PadSequence:
+    def __call__(self, batch):
+        # Sort batch by decreasing length
+        sorted_batch = sorted(batch, key=lambda x: x[0].shape[0], reverse=True)
+
+        # Pad sequences to max seq len
+        sequences = [x[0] for x in sorted_batch]
+        sequences_padded = torch.nn.utils.rnn.pad_sequence(sequences, batch_first=True)
+
+        # Get lengths and labels
+        lengths = torch.LongTensor([len(x) for x in sequences])
+        labels = torch.LongTensor([x[1] for x in sorted_batch])
+        return sequences_padded, lengths, labels
+
+
 class VideoDataset(Dataset):
     def __init__(
-        self, data_dir=os.path.join(os.path.dirname(__file__), "./vig"), transform=None
+        self,
+        data_dir=os.path.join(os.path.dirname(__file__), "./vig"),
+        transform=None,
+        frame_skip=10,
     ):
         self.data_dir = data_dir
         self.video_files = [f for f in os.listdir(data_dir) if f.endswith(".mp4")]
         self.transform = transform
-        (
-            self.annotations,
-            self.class_map,
-        ) = load_annotations_and_classmap()
+        self.frame_skip = frame_skip
+        self.annotations, self.class_map = load_annotations_and_classmap()
 
     def __len__(self):
         return len(self.video_files)
 
     def __getitem__(self, idx):
+        # Read video frames
         video_id = self.video_files[idx].split(".")[0]
         video_file = os.path.join(self.data_dir, self.video_files[idx])
         cap = cv2.VideoCapture(video_file)
         frames = []
+        frame_number = 0
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
-            frames.append(frame)
+            if frame_number % self.frame_skip == 0:
+                frames.append(frame)
+            frame_number += 1
         cap.release()
+        # print(f"{frame_number} frames; after skipping, {len(frames)} frames")
+
+        # Apply frame size normalization transformation
         if self.transform:
             frames = [self.transform(frame) for frame in frames]
-        return (
-            torch.stack(torch.Tensor(frames)),
-            self.annotations[video_id],
-            self.class_map[self.annotations[video_id]["vig_label"]],
-        )
+        video_frames_tensor = torch.Tensor(np.stack(frames))
+
+        # Pull video class
+        dataset_id = video_id_to_dataset_id(video_id)
+        video_annotations = self.annotations.get(dataset_id, {})
+        default_class_id = len(self.class_map)
+        video_class = video_annotations.get("class_id", default_class_id)
+        return video_frames_tensor, video_class
 
 
-def load_data(batch_size=32) -> tuple[DataLoader, DataLoader]:
+@lru_cache(maxsize=None)
+def frame_normalizer(height=240, width=320, grayscale=True):
+    # Composes a set of transform functions
+    def transform_composer(*fns):
+        def inner(frame: np.ndarray):
+            for fn in fns:
+                frame = fn(frame)
+            return frame
+
+        return inner
+
+    # Resizes a frame to a given (height, width)
+    def resizer(frame: np.ndarray):
+        return cv2.resize(frame, (width, height))
+
+    # Grayscales a frame
+    def grayscaler(frame: np.ndarray):
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    identity = lambda x: x
+    return transform_composer(
+        resizer,
+        grayscaler if grayscale else identity,
+    )
+
+
+def load_data(
+    batch_size=32, vid_height=240, vid_width=360, grayscale=True
+) -> tuple[DataLoader, DataLoader]:
     train_dir = os.path.join(os.path.dirname(__file__), "./vig_train")
     test_dir = os.path.join(os.path.dirname(__file__), "./vig_test")
-    train_data = DataLoader(VideoDataset(train_dir), batch_size=batch_size)
-    test_data = DataLoader(VideoDataset(test_dir), batch_size=batch_size)
+    train_data = DataLoader(
+        VideoDataset(
+            train_dir,
+            transform=frame_normalizer(
+                height=vid_height, width=vid_width, grayscale=grayscale
+            ),
+        ),
+        batch_size=batch_size,
+        collate_fn=PadSequence(),
+    )
+    test_data = DataLoader(
+        VideoDataset(
+            test_dir,
+            transform=frame_normalizer(
+                height=vid_height, width=vid_width, grayscale=grayscale
+            ),
+        ),
+        batch_size=batch_size,
+        collate_fn=PadSequence(),
+    )
     return train_data, test_data
-
 
 
 if __name__ == "__main__":
