148aa40419e09e45cb2f8c9026c05fae4e01f68c
Viswanath Chadalapaka <viswanath.chadalapaka@gmail.com>
2023-10-14 21:48:38 -0700

    update

--- models/pocan.py
+++ models/pocan.py
@@ -39,6 +39,7 @@ class VIGRnn(nn.Module):
         num_layers: int,
         video_fps: float = 24,
         audio_sampling_rate_khz: float = 90,
+        num_classes=None,
     ):
         """
         Creates an LSTM with a forward function which takes (batch_size, seq_len, input_size) tensors
@@ -49,20 +50,29 @@ class VIGRnn(nn.Module):
         """
         super(VIGRnn, self).__init__()
 
+        self.num_classes = num_classes
         # TODO for if we want to replicate hidden states from our cnn
         self.video_fps = video_fps
         self.audio_sampling_rate_khz = audio_sampling_rate_khz
         self.k = math.floor(self.audio_sampling_rate_khz / self.video_fps)
 
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc1_audio = nn.Linear(
-            hidden_size,
+        audio_space_output_shape = (
+            hidden_size + num_classes if num_classes else hidden_size
         )
+        self.fc1_audio = nn.Linear(audio_space_output_shape)
 
     def forward(self, x: torch.Tensor):
         # Input size: (batch_size, seq_len, dim)
         batch_size, seq_len, dim = x.shape
         x, h = self.lstm(x)
+        h = self.fc1_audio(h)
+
+        if self.num_classes:
+            # TODO do something here to pull out the predicted class for POCAN
+            pass
+
+        return h
 
 
 class POCAN(nn.Module):
