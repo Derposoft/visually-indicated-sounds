25eaf383cb78531ef15123f0846c9f1ba34b6d6e
Ehsan Khaligh <khalighehsan@gmail.com>
2023-11-05 23:08:37 -0800

    added vig

--- models/vig.py
+++ models/vig.py
@@ -1,41 +1,163 @@
+import cv2
+import os
+from glob import glob
 import torch
 import torch.nn as nn
 import librosa
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+from torchvision import models, transforms
+import warnings
+import torch.optim as optim
+warnings.filterwarnings('ignore')
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import numpy as np
+import librosa
+import torch.nn.functional as F
 
 
-class VIG(nn.Module):
+class CustomFeatureExtractor(nn.Module):
+    def __init__(self, input_channels=30):
+        super(CustomFeatureExtractor, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+        # Add more layers as needed
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        # Forward pass through additional layers
+        return x
+
+class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(VIG, self).__init__()
-        self.alexnet = torch.hub.load("pytorch/vision", "alexnet", pretrained=True)
+        super(LSTMModel, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
 
-    def forward(self, x, _):
-        cochleagrams = []
-
-        x = self.alexnet(x)
-
-        cochleagrams = self.generate_cochleagram(x)
-        audio_waveform = self.inverse_cochleagram_to_audio(cochleagrams)
-        audio_channel = audio_waveform[0, 0, :]
-        audio_channel = audio_channel.reshape(-1)
-        combined_audio_data = torch.cat([combined_audio_data, audio_channel])
-
+    def forward(self, x):
         out, _ = self.lstm(x)
-        out = self.fc(out[:, -1, :])
+
+        # Check the shape of the out tensor
+        if out.ndim == 3:
+            # If it's a 3D tensor, extract the last output
+            out = self.fc(out[:, -1, :])
+        elif out.ndim == 2:
+            # If it's a 2D tensor, add a time step dimension and then extract the last output
+            out = self.fc(out[:, -1, :].unsqueeze(1))
+        else:
+            raise ValueError("Unexpected input tensor shape")
+
         return out
 
-    def generate_cochleagram(self, features):
-        with torch.no_grad():
-            features = features.view(1, 256, -1)
-            cochleagram = self.lstm(features)
-        return cochleagram.squeeze(1)
+class vig(nn.Module):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, frame_rate=30):
+        super(vig, self).__init__()
+        self.num_classes = num_classes
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.frame_rate = frame_rate
 
-    def inverse_cochleagram_to_audio(self, cochleagram):
-        inverted_audio = librosa.griffinlim(
-            np.array(cochleagram), hop_length=512, win_length=256
-        )
-        return inverted_audio
+        # Initialize custom feature extractor
+        self.feature_extractor = CustomFeatureExtractor(input_channels=30)
+
+
+        # Initialize LSTM model
+        self.lstm_model = self._initialize_lstm_model()
+
+    def _initialize_feature_extractor(self):
+        feature_extractor = models.alexnet(pretrained=True)
+        # Remove the last classification layer to get features
+        feature_extractor = nn.Sequential(*list(feature_extractor.children())[:-1])
+        return feature_extractor
+
+    def _initialize_lstm_model(self):
+        lstm_model = LSTMModel(self.input_size, self.hidden_size, self.num_layers, self.num_classes)
+        return lstm_model
+
+    def generate_cochleagram(self, features_all_frames):
+        # Reshape the input features
+        feature_array = features_all_frames.view(-1, self.input_size)
+
+        # Convert features to cochleagram
+        feature_array = feature_array.cpu().detach().numpy()
+        cochleagram = librosa.feature.melspectrogram(S=feature_array.T)
+        return cochleagram
+
+    def forward(self, video_frames, audio_waves):
+        # Extract video features with AlexNet
+        video_features = self.feature_extractor(video_frames)
+
+        print(video_features.shape)
+        batch_size, seq_len, height, width = video_features.size()
+        video_features = video_features.view(batch_size, seq_len, height * width)
+
+        # Forward pass through video LSTM
+        video_output = self.lstm_model(video_features)
+
+        # Forward pass audio_waves through audio LSTM or other audio model if needed
+        audio_output = self.audio_model(audio_waves)
+
+        # Combine the video and audio features as needed
+        combined_features = self.combine_features(video_output, audio_output)
+
+        return combined_features
+
+    def audio_model(self, audio_waves):
+        # Implement your audio feature extraction or model here
+        # This could be another LSTM or any other model for audio data
+        pass
+
+    def combine_features(self, video_features, audio_features):
+        # Implement how you want to combine video and audio features
+        # This can be a simple concatenation, element-wise addition, etc.
+        pass
+
+
+"""
+class AlexNetFeatureExtractor(nn.Module):
+    def __init__(self):
+        super(AlexNetFeatureExtractor, self).__init__()
+        self.alexnet = torch.hub.load('pytorch/vision', 'alexnet', pretrained=True)
+        self.features = self.alexnet.features
+
+    def forward(self, x):
+        return self.features(x)
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTMModel, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+
+        # Check the shape of the out tensor
+        if out.ndim == 3:
+            # If it's a 3D tensor, extract the last output
+            out = self.fc(out[:, -1, :])
+        elif out.ndim == 2:
+            # If it's a 2D tensor, add a time step dimension and then extract the last output
+            out = self.fc(out[:, -1, :].unsqueeze(1))
+        else:
+            raise ValueError("Unexpected input tensor shape")
+
+        return out
+
+def generate_cochleagram(features_all_frames):
+    # Reshape the input features
+    feature_array = features_all_frames.view(-1, 9216)  # Assuming the shape is (time frames, features)
+
+    # Convert features to cochleagram
+    feature_array = feature_array.cpu().detach().numpy()  # Convert to NumPy array
+    cochleagram = librosa.feature.melspectrogram(S=feature_array.T)  # Transpose for the correct shape
+
+    return cochleagram
+"""
--- train.py
+++ train.py
@@ -4,8 +4,100 @@ import torch.optim as optim
 
 import data.utils as utils
 from models.pocan import POCAN
-from models.vig import VIG
+from models.vig import vig
 from models.foleygan import foleygan
+
+def vig_preprocess_frame(frame):
+    frame = cv2.resize(frame, (227, 227))
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    frame = frame.astype(np.float32) / 255.0
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    frame = (frame - mean) / std
+    frame = torch.from_numpy(frame).permute(2, 0, 1).unsqueeze(0).float()
+    return frame
+
+def vig_extract_frames(video_path, frame_rate):
+    frames = []
+    cap = cv2.VideoCapture(video_path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    return frames
+
+def vig_train(lstm_model, num_epochs=10, frame_rate=30):
+
+    combined_audio_data = np.array([])
+    mp4_directory = 'data/vig_train/'
+    mp4_files = glob(os.path.join(mp4_directory, '*.mp4'))
+
+    count = 0
+    # Training loop
+    for video_path in mp4_files:
+        print(video_path)
+
+        frames = vig_extract_frames(video_path, frame_rate)
+
+        # Initialize AlexNet feature extractor
+        feature_extractor = AlexNetFeatureExtractor()
+
+        # Process each frame and generate the cochleagram
+        features_all_frames = []
+        print("frames.shape:", len(frames))
+        for frame in frames:
+            frame = vig_preprocess_frame(frame)
+            features = feature_extractor(frame)
+            features_all_frames.append(features)
+
+        # Train LSTM
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(lstm_model.parameters(), lr=0.001)
+
+        # Convert the list of features into a single tensor
+        print("len(features_all_frames):", len(features_all_frames))
+        print("len(features_all_frames[0]):", len(features_all_frames[0]))
+
+        features_all_frames = torch.stack(features_all_frames)
+        best_model = None
+
+        for epoch in range(num_epochs):
+            lstm_model.train()
+            optimizer.zero_grad()
+
+            # Pass features of all frames through LSTM
+            print("features_all_frames.shape:", features_all_frames.shape)
+            # Reshape features_all_frames to match the expected input size
+            features_all_frames = features_all_frames.view(1, -1, input_size)  # Assuming batch size of 1
+            print("features_all_frames.shape:", features_all_frames.shape)
+            outputs = lstm_model(features_all_frames)
+
+            output_size = torch.Size([1, 128])
+            num_classes = 15  # Number of classes
+            labels = torch.randint(0, num_classes, output_size)
+
+            print("outputs.shape:", outputs.shape)
+            print("labels.shape:", labels.shape)
+
+            outputs = outputs.to(torch.float)
+            labels = labels.to(torch.float)
+
+            loss = criterion(outputs, labels)
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+            print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
+
+
+        print("features_all_frames.shape:", features_all_frames.shape)
+        cochleagram = generate_cochleagram(features_all_frames)
+        print("cochleagram:", cochleagram)
+
+        if count == 1:
+            break
+        else:
+            count = count + 1
 
 
 def train(model, train_dataloader, criterion, opt, num_epochs=10, verbose=False):
@@ -13,7 +105,14 @@ def train(model, train_dataloader, criterion, opt, num_epochs=10, verbose=False)
         running_loss = 0.0
         for video_frames, audio_waves, labels in train_dataloader:
             opt.zero_grad()
+
+            print("video_frames.shape:", video_frames.shape)
+            print("audio_waves.shape:", audio_waves.shape)
+
             outputs = model(video_frames, audio_waves)
+
+            print("outputs.shape:", outputs.shape)
+            print("outputs:", outputs)
 
             # Custom losses by model
             if isinstance(model, POCAN):
@@ -96,16 +195,34 @@ if __name__ == "__main__":
         loss_function = nn.CrossEntropyLoss()
         opt = optim.SGD(model.parameters(), lr=0.01)
     elif config.model == "vig":
-        input_size = 1
-        hidden_size = 64
+        #input_size = 9216 #initial
+        #hidden_size = 512 #initial
+        #num_layers = 2 #initial
+        #output_size = 128 #initial
+
+        #model = LSTMModel(input_size, hidden_size, num_layers, output_size) #initial
+
+        input_size = 256
+        hidden_size = 512
         num_layers = 2
-        num_classes = 10
-        model = VIG(input_size, hidden_size, num_layers, num_classes)
+        output_size = 128
+
+        model = vig(num_classes, input_size, hidden_size, num_layers)
+
+        print(model)
+
         loss_function = nn.CrossEntropyLoss()
         opt = optim.SGD(model.parameters(), lr=0.01)
+
     assert model != None
 
     # Train models
-    train(
-        model, train_dataloader, loss_function, opt, num_epochs=epochs, verbose=verbose
-    )
+    if config.model != "vig":
+        train(
+            model, train_dataloader, loss_function, opt, num_epochs=epochs, verbose=verbose
+        )
+    else:
+        train(
+            model, train_dataloader, loss_function, opt, num_epochs=10, verbose=verbose
+        )
+        #vig_train(model, num_epochs=10, frame_rate=30) #initial
