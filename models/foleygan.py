3a4db6dc686a52a7ad30ec11b669a57bbbdffba8
Viswanath Chadalapaka <viswanath.chadalapaka@gmail.com>
2023-11-15 22:19:21 -0800

    foleygan loss update

--- models/foleygan.py
+++ models/foleygan.py
@@ -30,8 +30,13 @@ class FoleyGAN(nn.Module):
         GAN_OUTPUT_DIM = 128
         MULTI_SCALE_NUM_FRAMES = 8
         NUM_FRAMES = 3
-        self.sequence_length = int((((n_fft//2)+1)*(audio_sample_rate_out/NUM_FRAMES))/batch_size)
-        self.stft_downsample = int((((n_fft//2)+1)*((audio_sample_rate_out/NUM_FRAMES)-1))/batch_size)
+        self.sequence_length = int(
+            (((n_fft // 2) + 1) * (audio_sample_rate_out / NUM_FRAMES)) / batch_size
+        )
+        self.stft_downsample = int(
+            (((n_fft // 2) + 1) * ((audio_sample_rate_out / NUM_FRAMES) - 1))
+            / batch_size
+        )
         self.n_fft = n_fft
         self.biggan_z_dim = biggan_z_dim
 
@@ -53,7 +58,7 @@ class FoleyGAN(nn.Module):
             n_fft,
             z_dim=biggan_z_dim,
             gan_output_dim=GAN_OUTPUT_DIM,
-            audio_sample_rate_out=audio_sample_rate_out
+            audio_sample_rate_out=audio_sample_rate_out,
         )
         self.istft = audiotransforms.InverseSpectrogram(n_fft)
         self.stft = audiotransforms.Spectrogram(n_fft)
@@ -63,8 +68,7 @@ class FoleyGAN(nn.Module):
         self.toggle_freeze_discriminator()
         self.discrim_loss_fn = nn.HingeEmbeddingLoss()
         self.x_pred = None
-        self.x_discrim_real = None
-        self.x_discrim_imag = None
+        self.x_discrim = None
 
     def forward(self, x, _):
         batch_size = x.shape[0]
@@ -77,77 +81,62 @@ class FoleyGAN(nn.Module):
         noise = torch.rand(batch_size, self.biggan_z_dim)
         x = self.biggan(noise, x_class, x_spectrogram)
 
+        # Get discriminator output
+        x_real, x_imag = x.real, x.imag
+        x_discrim = torch.cat([x_real, x_imag], dim=0)
+        x_discrim = x_discrim.reshape(x_discrim.shape[0], -1)
+        x_discrim = x_discrim.unsqueeze(-1)
+        self.x_discrim = self.discriminator(x_discrim)
+
         # Create audio wave via istft
         x = x.permute(0, 2, 1)
-        z = x.reshape([x.shape[0], -1])
-        #z = torch.cat((z.real, z.imag))
-        #z = z[:, :, None]
-        #z = z.permute(2, 1, 0)
-        #self.x_discrim = self.discriminator(z)
-        z_real = z.real
-        z_real  = z_real[:, :, None]
-        z_real = z_real.permute(2, 1, 0)
-        self.x_discrim_real = self.discriminator(z_real)
-
-        z_imag = z.real
-        z_imag  = z_imag[:, :, None]
-        z_imag = z_imag.permute(2, 1, 0)
-        self.x_discrim_imag = self.discriminator(z_imag)
-
         x = self.istft(x)
-        x = x.reshape([*x.shape, 1])  # (bs, seq_len, 1)
-
         self.x_pred = x
         return x
 
     def toggle_freeze_generator(self):
         for param in self.cnn.parameters():
-            param.requires_grad = not(param.requires_grad)
-
+            param.requires_grad = not (param.requires_grad)
         for param in self.mtrn.parameters():
-            param.requires_grad = not(param.requires_grad)
-
+            param.requires_grad = not (param.requires_grad)
         for param in self.trn.parameters():
-            param.requires_grad = not(param.requires_grad)
-
+            param.requires_grad = not (param.requires_grad)
         for param in self.biggan.parameters():
-            param.requires_grad = not(param.requires_grad)
+            param.requires_grad = not (param.requires_grad)
 
     def toggle_freeze_discriminator(self):
         for param in self.discriminator.parameters():
-            param.requires_grad = not(param.requires_grad)
+            param.requires_grad = not (param.requires_grad)
 
     def loss(self, outputs, _, audiowaves):
         """Function starts with discriminator frozen, generator unfrozen"""
-        batch_size, pred_seq_len, dimension = outputs.shape
+        batch_size, pred_seq_len = outputs.shape
+        audiowaves = F.interpolate(
+            audiowaves.unsqueeze(1),
+            size=pred_seq_len,
+            mode="linear",
+            align_corners=False,
+        )[0]
 
-        # Train discriminator on negative and positive example
+        # Discriminator loss
         self.toggle_freeze_discriminator()
         self.toggle_freeze_generator()
-
-        # Negative example
-        loss_discriminator = self.discrim_loss_fn(
-            (self.x_discrim_real + self.x_discrim_imag), torch.zeros((batch_size, 1))
-        )
-        loss_discriminator.backward(retain_graph=True)
-
-        # Positive example
-        target_size = self.stft_downsample
-        audiowaves_downsampled = F.interpolate(audiowaves.unsqueeze(1), size=target_size, mode='linear', align_corners=False)
-        audiowaves_downsampled = audiowaves_downsampled.squeeze(1)
-        spectrogram = self.stft(audiowaves_downsampled)
-  
+        spectrogram = self.stft(audiowaves)
         spectrogram = spectrogram.reshape([spectrogram.shape[0], -1])
-        spectrogram = spectrogram[:, :, None]
-        spectrogram = spectrogram.permute(2, 1, 0)
-
-        x_discrim_raw = self.discriminator(spectrogram)
-        loss_discriminator = self.discrim_loss_fn(
-            x_discrim_raw, torch.ones((batch_size, 1))
+        spectrogram = spectrogram[:, :, None].permute(2, 1, 0)
+        x_discrim_pos = self.discriminator(spectrogram)
+        loss_discrim_pos = self.discrim_loss_fn(
+            x_discrim_pos, torch.ones((batch_size, 1))
         )
-        loss_discriminator.backward(retain_graph=True)
+        loss_discrim_neg = self.discrim_loss_fn(
+            self.x_discrim, torch.zeros((batch_size, 1))
+        )
+        loss_discrim = loss_discrim_pos + loss_discrim_neg
+        loss_discrim.backward()
 
+        # Generator output
         self.toggle_freeze_discriminator()
         self.toggle_freeze_generator()
-        loss_generator = modules.calculate_audiowave_loss(audiowaves, outputs)
+        loss_generator = 1 - loss_discrim
+        loss_generator.backward()
         return loss_generator
